55c457c6c3fca4a3e914b5c81d7442a7acca6220
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-19 23:23:29 +0600

    Update 5.py

--- CSE110/Assignment 2/5.py
+++ CSE110/Assignment 2/5.py
@@ -5,3 +5,10 @@ Created on Wed Oct 19 23:01:56 2022
 @author: xyzub
 """
 
+x=0
+counter=0
+for i in range(0,601):
+    if i % 7 == 0 and i % 9 == 0:
+        counter+=i
+        if i % 7 == 0 or i % 9 == 0:
+            counter2=counter2
