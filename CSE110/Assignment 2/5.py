3dbad10937869e13cee37bd939480e1efc4c8bae
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-19 23:35:17 +0600

    Update 5.py

--- CSE110/Assignment 2/5.py
+++ CSE110/Assignment 2/5.py
@@ -8,7 +8,8 @@ Created on Wed Oct 19 23:01:56 2022
 x=0
 counter=0
 for i in range(0,601):
+    if i % 7 == 0 or i % 9 == 0:
+        x+=i
     if i % 7 == 0 and i % 9 == 0:
-        counter+=i
-        if i % 7 == 0 or i % 9 == 0:
-            
+        counter=counter+i
+print(x - counter)
