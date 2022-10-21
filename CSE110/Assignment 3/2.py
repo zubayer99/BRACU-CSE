7d07a56ced973551dbc6c2b99ea32ce5a5cbe39e
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-21 18:53:11 +0600

    Update 2.py

--- CSE110/Assignment 3/2.py
+++ CSE110/Assignment 3/2.py
@@ -7,4 +7,9 @@ Created on Fri Oct 21 14:28:33 2022
 
 word=input()
 lenword=len(word)
-if 
+if lenword < 4:
+    print(word)
+elif lenword > 3:
+    print(word + "er")
+if word.endswith("er"):
+    print(word.replace("er", "est"))
