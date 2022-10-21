95bcff459c0a9412bacdb0f20e3ff212570bb753
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-21 18:39:05 +0600

    Update 2.py

--- CSE110/Assignment 3/2.py
+++ CSE110/Assignment 3/2.py
@@ -6,9 +6,5 @@ Created on Fri Oct 21 14:28:33 2022
 """
 
 word=input()
-for i in range(len(word)+1):
-    if len(word) < 4:
-        print(word)
-    elif len(word) > 3:
-        print(word + "er")
-    elif len(word)
+lenword=len(word)
+if 
