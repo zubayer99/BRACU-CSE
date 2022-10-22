bc9d921bc4a6ac2763da3ff99c5e77fd9e2e2fc5
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-22 15:50:22 +0600

    Update 4.py

--- CSE110/Assignment 3/4.py
+++ CSE110/Assignment 3/4.py
@@ -5,10 +5,5 @@ Created on Fri Oct 21 20:02:45 2022
 @author: xyzub
 """
 
-string=input()
-for i in range(0,len(string)):
-      x=ord(string[i])
-      if chr(x+1) > 122:
-          
-      print(chr(x+1))
+
     
