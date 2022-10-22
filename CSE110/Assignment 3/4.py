350c92a589a3c41c54a55e6f2144549c1fb583f9
Zubayer Hassan <xyzubayer@gmail.com>
2022-10-22 15:53:13 +0600

    Update 4.py

--- CSE110/Assignment 3/4.py
+++ CSE110/Assignment 3/4.py
@@ -5,5 +5,15 @@ Created on Fri Oct 21 20:02:45 2022
 @author: xyzub
 """
 
+u_input=input()
 
+for i in u_input:
+    ascii_val = ord(i)
+    ascii_val +=1
+    
+    if ascii_val > 122:
+        ascii_val = 97
+    
+    ch = chr(ascii_val)
+    print(ch, end = "")
     
